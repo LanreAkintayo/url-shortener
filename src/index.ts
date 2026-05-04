f39dac7cb5bdf4682f8afb36af83c7c2ceca5f54
LanreAkintayo <akintayolanre2019@gmail.com>
2026-05-04 09:44:48 +0100

    feat: update the index to spin up kgs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import app from "./app";
 import { connectRedis } from "./config/redis";
+import { startKGSWorker } from "./workers/kgs.worker";
 
 const port = process.env.PORT || 3000;
 
 const startServer = async () => {
-    await connectRedis();
+  await connectRedis();
 
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+  // Start the Express server
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+  // Start the background worker for key generation. It is done this way because a dedicated bakcground worker server is not free on render but I'm aware of the multi-server issue, and other downsides of this approach
+  await startKGSWorker();
+  console.log("KGS Worker started successfully.");
 };
 
 startServer();
